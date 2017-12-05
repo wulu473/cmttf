0dfbcf5af99bbc74ca8274c20be8045406562f31
Lukas Wutschitz <lw473@cam.ac.uk>
2017-12-05 12:48:05 +0000

    Reduce boost install logging output
    Travis CI only allows logfiles < 4MB and boost logs a lot while
    installing. Disable boost logging

--- travis-scripts/install-boost.sh
+++ travis-scripts/install-boost.sh
@@ -5,7 +5,7 @@ wget -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.
 tar xzvf boost_1_64_0.tar.gz
 cd boost_1_64_0
 ./bootstrap.sh --with-libraries=program_options,test
-./b2 variant=release
-sudo ./b2 install
+./b2 -d0 variant=release
+sudo ./b2 -d0 install
 cd $WD
 
