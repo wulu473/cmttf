97e43527195db07ed675095691984e8006d97fe0
Lukas Wutschitz <lw473@cam.ac.uk>
2017-12-05 14:06:13 +0000

    Add boost log to travis install script

--- travis-scripts/install-boost.sh
+++ travis-scripts/install-boost.sh
@@ -4,7 +4,7 @@ cd /tmp
 wget -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.gz/download
 tar -xf boost_1_64_0.tar.gz
 cd boost_1_64_0
-./bootstrap.sh --with-libraries=program_options,test
+./bootstrap.sh --with-libraries=program_options,test,log
 ./b2 -d0 variant=release
 sudo ./b2 -d0 install
 cd $WD
