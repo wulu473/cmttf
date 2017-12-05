b86b7f7788f773b9e4c99c22de63cbe89418dc9f
Lukas Wutschitz <lw473@cam.ac.uk>
2017-12-05 12:39:25 +0000

    Fix boost install script

--- travis-scripts/install-boost.sh
+++ travis-scripts/install-boost.sh
@@ -5,6 +5,7 @@ wget -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.
 tar xzvf boost_1_64_0.tar.gz
 cd boost_1_64_0
 ./bootstrap.sh --with-libraries=program_options,test
-sudo ./boostrap install variant=release
+./b2 variant=release
+sudo ./b2 install
 cd $WD
 
